54f37c53ed97f39f117d67f1e5e19d7100a83f43
unknown <fatlum@urbanway.net>
2021-01-14 14:09:26 +0100

    added Seeder

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -93,7 +93,7 @@ class UserController extends Controller
             'first_name' => ['required', 'string', 'max:255'],
             'last_name' => ['required', 'string', 'max:255'],
             'phone_number' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'email' => ['required', 'string', 'email', 'max:255',],
             'password' => ['required', 'string', 'min:8',],
         ]);
 
